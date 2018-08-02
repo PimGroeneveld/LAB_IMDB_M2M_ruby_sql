80dc8d2f8984c0dcd914209dbc5a9f71a6c963db
Pim <pimgroeneveld92@gmail.com>
2018-08-02 15:28:10 +0100

    mvp + attempt budget

--- models/movie.rb
+++ models/movie.rb
@@ -3,20 +3,20 @@ require_relative("../db/sql_runner")
 class Movie
 
   attr_reader :id
-  attr_accessor :title, :genre   #, :budget
+  attr_accessor :title, :genre, :budget
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @title = options['title']
     @genre = options['genre']
-    # @budget = options['budget'].to_i
+    @budget = options['budget']
   end
 
   # Create
   def save()
-    sql = "INSERT INTO movies(title, genre) VALUES ($1, $2)
+    sql = "INSERT INTO movies(title, genre, budget) VALUES ($1, $2, $3)
     RETURNING id"
-    values = [@title, @genre]
+    values = [@title, @genre, @budget]
     location = SqlRunner.run(sql, values).first
     @id = location['id'].to_i
   end
@@ -32,8 +32,8 @@ class Movie
 
   #Update
   def update()
-    sql = "UPDATE movies SET (title, genre) = ($1, $2) WHERE id = $3"
-    values = [@title, @genre, @id]
+    sql = "UPDATE movies SET (title, genre, budget) = ($1, $2, $3) WHERE id = $4"
+    values = [@title, @genre, @budget, @id]
     SqlRunner.run(sql, values)
   end
 
@@ -58,12 +58,12 @@ class Movie
     return stars.map {|star| Star.new(star)}
   end
 
-  # def budget_count()
-  #   sql = "SELECT COUNT movies.* FROM movies INNER JOIN castings ON movies.budget = castings.fee WHERE castings.movie_id = $1"
-  #   values = [@id]
-  #   budget_count = budget { |fee| fee }
-  #   return budget_count
-  # end
+  def budget_count()
+    sql = "SELECT COUNT movies.* FROM movies INNER JOIN castings ON movies.budget -= castings.fee WHERE castings.movie_id = $1"
+    values = [@id]
+    budget_count = budget { |fee| fee }
+    return budget_count
+  end
 
 
 end
